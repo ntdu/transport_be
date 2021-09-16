6297a8b965caa98f27a961395a358eae2e49a622
unknown <ntdu198@gmail.com>
2021-09-16 23:00:14 +0700

    Update api register

--- transport_server/customer/views/views_user.py
+++ transport_server/customer/views/views_user.py
@@ -1,6 +1,12 @@
 from django.contrib.auth.decorators import login_required, permission_required 
+from rest_framework.decorators import authentication_classes, permission_classes, api_view
 from django.http import JsonResponse, HttpResponse
+from apiHelper.apiHelper import ApiHelper
 
+
+@api_view(['GET'])
+# @authentication_classes([TokenAuthentication])
+# @permission_classes([IsAuthenticated])
 def register(request):  
     try:
         # form =  ApiHelper.getData(request)
@@ -11,7 +17,7 @@ def register(request):
         # user.set_password(password)   
         # user.save()
 
-        return HttpResponse("ok")
+        return ApiHelper.Response_ok('ok')
     except Exception as e:
         print(e)
         return ApiHelper.Response_error()
