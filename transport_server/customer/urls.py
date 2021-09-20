46db0086d0653cb180928021b817a1f0d424df8c
unknown <ntdu198@gmail.com>
2021-09-20 19:33:00 +0700

    Update App config

--- transport_server/customer/urls.py
+++ transport_server/customer/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 
-app_name = 'biker'
+app_name = 'customer'
 urlpatterns = [
   path('register', views.register , name='register'),
   path('login', views.login , name='login'),
