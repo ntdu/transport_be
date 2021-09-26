b7466af04210a70ecd8cb05b8c6e058fd53ddc10
unknown <ntdu198@gmail.com>
2021-09-26 16:18:58 +0700

    Upgrade from sqlite to postgres

--- transport_server/transport_server/settings.py
+++ transport_server/transport_server/settings.py
@@ -128,13 +128,23 @@ CACHES = {
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dc51muauvtg25i',
+        'USER': 'lfnjnvztyjrpud',
+        'PASSWORD': 'a199f68fe3ab19f29e8dc4b3479514c748c82d4a406b1ff9dd5b71716069425c',
+        'HOST': 'ec2-54-146-84-101.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
