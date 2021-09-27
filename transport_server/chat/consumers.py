8c24abc3ad9f0f0a80576dad02295a91aff02752
unknown <ntdu198@gmail.com>
2021-09-27 19:24:15 +0700

    Add type delivery biker chosen event

--- transport_server/chat/consumers.py
+++ transport_server/chat/consumers.py
@@ -164,8 +164,22 @@ class ChatConsumer(WebsocketConsumer):
                 }
             )
 
+        elif type == 'DELIVERY_BIKER_CHOSEN_EVENT':
+            token = text_data_json['message']['token']
+            customer = Customer.objects.filter(login_account=Token.objects.get(key=token).user).first()
 
+            data = text_data_json['message']['data']
+            biker = data['biker']
+            price = data['price']
 
+            message = price
+            async_to_sync(self.channel_layer.group_send)(
+                self.room_group_name,
+                {
+                    'type': 'chat_message',
+                    'message': message
+                }
+            )
 
     # Receive message from room group
     def chat_message(self, event):
